9e00a15f09f164d7733019143e54890fb459d347
Kizito Nwose <kizitonwose@gmail.com>
2017-10-19 12:14:48 +0100

    Add "time-android" module for Android-related extensions.

---
+++ time-android/src/main/kotlin/com/kizitonwose/timeandroid/AndroidExtensions.kt
@@ -0,0 +1,15 @@
+package com.kizitonwose.timeandroid
+
+import android.os.Handler
+import com.kizitonwose.time.*
+
+/**
+ * Created by Kizito Nwose on 19/10/2017
+ */
+
+fun Handler.postDelayed(r: Runnable, delay: Interval<TimeUnit>)
+        = postDelayed(r, delay.inMilliseconds.value.toLong())
+
+fun Handler.postDelayed(r: () -> Unit, delay: Interval<TimeUnit>)
+        = postDelayed(r, delay.inMilliseconds.value.toLong())
+
